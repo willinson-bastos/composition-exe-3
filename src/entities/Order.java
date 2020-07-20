08cfc108467dfc4a1b38c04a20c2e8b2f5d1f7da
Willinson Bastos <willinson.bastos@gmail.com>
2020-07-20 10:40:42 -0300

    Atualiações na classe 'Order'

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -11,6 +11,51 @@ public class Order {
 	private OrderStatus status;
 	
 	private List<OrderItem> items = new ArrayList<>();
+	private Client client;
 	
+	//construtor
+	public Order() {
+		
+	}
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
 	
+	//get & set
+	public Date getMoment() {
+		return moment;
+	}
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	//metodos
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
+	
+	public double totalValue() {
+		double sum=0;
+		for(OrderItem i: items) {
+			sum += i.subTotal();
+		}
+		return sum;
+	}
 }
